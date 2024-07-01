3546a82fd20f76d83b3a1726bc5abac9df13c06a
amaechijude <ugwuamaechi5@gmail.com>
2024-07-01 18:19:47 +0100

    2nd

--- git.sh
+++
@@ -1,7 +0,0 @@
-echo "# hng11" >> README.md
-git init
-git add README.md
-git commit -m "first commit"
-git branch -M main
-git remote add origin https://github.com/amaechijude/hng11.git
-git push -u origin main
